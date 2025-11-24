944305ad5cc34e6fb32c3af6bb3639a623bc8989
Harrison Billings <harrison.billings@pelo.tech>
2025-11-24 10:08:45 -0700

    docs: comments in the vmdi config reader

--- internal/vm-disk-image/config/vmdi-controller-config-reader.go
+++ internal/vm-disk-image/config/vmdi-controller-config-reader.go
@@ -22,12 +22,16 @@ type VMDiskImageControllerConfig struct {
 // This function will allow us to get the required config variables from the environment.
 // Locally this is your "env" and in production these values will come from a configmap
 func LoadVMDIControllerConfigFromEnv() VMDiskImageControllerConfig {
+	// The max amount of VMDIs we can have syncing at one time.
 	concurrency := corecfg.GetIntEnvOrDefault("CONCURRENCY", defaultConcurrency)
 
+	// How many times we will retry a failed sync.
 	retryLimit := corecfg.GetIntEnvOrDefault("RETRY_LIMIT", defaultRetryLimit)
 
+	// How long we want to wait before trying to resync a failed VMDI.
 	retryBackoffDuration := corecfg.GetDurationEnvOrDefault("RETRY_BACKOFF_DURATION", defaultBackoffDuration)
 
+	// How long we will let a VMDI sit in syncing status.
 	maxSyncDuration := corecfg.GetDurationEnvOrDefault("MAX_SYNC_DURATION", defaultMaxSyncDuration)
 
 	return VMDiskImageControllerConfig{
